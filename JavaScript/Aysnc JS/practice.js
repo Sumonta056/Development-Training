897047270b0ef2d16e166618eacba1cf4757d726
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2025-01-13 17:41:46 +0600

    feat: async promis

---
+++ JavaScript/Aysnc JS/practice.js
@@ -0,0 +1,31 @@
+const MAX_PRIME = 1000000;
+
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n > 1;
+}
+
+const random = (max) => Math.floor(Math.random() * max);
+
+async function generatePrimes(quota) {
+  const primes = [];
+  while (primes.length < quota) {
+    const candidate = random(MAX_PRIME);
+    if (isPrime(candidate)) {
+      primes.push(candidate);
+    }
+  }
+  return primes.length;
+}
+
+async function main(val) {
+  const value = await generatePrimes(val);
+  console.log(value);
+}
+
+main(10054545);
+console.log("Hello");
